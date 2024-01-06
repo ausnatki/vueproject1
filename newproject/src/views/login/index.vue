441c7e09dbfbc6e082a0ad6b43add365375624bc
ausnatki <109503108+ausnatki@users.noreply.github.com>
2024-01-06 23:27:04 +0800

    das

--- newproject/src/views/login/index.vue
+++ newproject/src/views/login/index.vue
@@ -54,14 +54,14 @@
 
       <div style="position:relative">
         <div class="tips">
-          <span>{{ $t('login.username') }} : admin</span>
-          <span>{{ $t('login.password') }} : {{ $t('login.any') }}</span>
+          <span>{{ $t('login.username') }} : kkk</span>
+          <span>{{ $t('login.password') }} : 123456</span>
         </div>
         <div class="tips">
           <span style="margin-right:18px;">
-            {{ $t('login.username') }} : editor
+            {{ $t('login.username') }} : ccc
           </span>
-          <span>{{ $t('login.password') }} : {{ $t('login.any') }}</span>
+          <span>{{ $t('login.password') }} : 123456</span>
         </div>
 
         <el-button class="thirdparty-button" type="primary" @click="showDialog=true">
